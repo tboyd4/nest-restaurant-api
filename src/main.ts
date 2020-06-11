66a2e5cba6a0d60889149a6710263e4b3e2ed66d
Tyler Boyd <tylerb@theboydprojects.com>
2020-06-11 10:03:31 -0500

    enabled cors through main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT);
 }
